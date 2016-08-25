fcb5abacb860d17b2242d750f799c04b8e960cdf
Ky0_D1a5 <kyotsuyoi@hotmail.com>
2016-08-25 08:53:51 -0300

    test

---
+++ GIPPBackup/frmBackup.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace GIPPBackup
+{
+    partial class frmBackup
+    {
+        
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            //if (disposing && (components != null))
+            //{
+            //     components.Dispose();
+            // }
+            //base.Dispose(disposing);
+            this.Visible = false;
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBackup));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblBakData = new System.Windows.Forms.Label();
+            this.lblBakFiles = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFilesPath = new System.Windows.Forms.TextBox();
+            this.txtBakPath = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDataBkp = new System.Windows.Forms.Button();
+            this.btnFileBkp = new System.Windows.Forms.Button();
+            this.lblFiles = new System.Windows.Forms.Label();
+            this.cmbBkpTime = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "GIPP Backup";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data do ultimo backup de dados:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Data do ultimo backup de arquivos:";
+            // 
+            // lblBakData
+            // 
+            this.lblBakData.AutoSize = true;
+            this.lblBakData.Location = new System.Drawing.Point(195, 13);
+            this.lblBakData.Name = "lblBakData";
+            this.lblBakData.Size = new System.Drawing.Size(30, 13);
+            this.lblBakData.TabIndex = 3;
+            this.lblBakData.Text = "Data";
+            // 
+            // lblBakFiles
+            // 
+            this.lblBakFiles.AutoSize = true;
+            this.lblBakFiles.Location = new System.Drawing.Point(195, 36);
+            this.lblBakFiles.Name = "lblBakFiles";
+            this.lblBakFiles.Size = new System.Drawing.Size(30, 13);
+            this.lblBakFiles.TabIndex = 4;
+            this.lblBakFiles.Text = "Data";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Caminho para copiar arquivos/dados:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(185, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Caminho para gravar arquivos/dados:";
+            // 
+            // txtFilesPath
+            // 
+            this.txtFilesPath.Location = new System.Drawing.Point(198, 61);
+            this.txtFilesPath.Name = "txtFilesPath";
+            this.txtFilesPath.Size = new System.Drawing.Size(192, 20);
+            this.txtFilesPath.TabIndex = 7;
+            // 
+            // txtBakPath
+            // 
+            this.txtBakPath.Location = new System.Drawing.Point(198, 87);
+            this.txtBakPath.Name = "txtBakPath";
+            this.txtBakPath.Size = new System.Drawing.Size(192, 20);
+            this.txtBakPath.TabIndex = 8;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(295, 113);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(95, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Gravar caminhos";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDataBkp
+            // 
+            this.btnDataBkp.Location = new System.Drawing.Point(295, 8);
+            this.btnDataBkp.Name = "btnDataBkp";
+            this.btnDataBkp.Size = new System.Drawing.Size(95, 23);
+            this.btnDataBkp.TabIndex = 10;
+            this.btnDataBkp.Text = "Gerar Backup";
+            this.btnDataBkp.UseVisualStyleBackColor = true;
+            this.btnDataBkp.Click += new System.EventHandler(this.btnDataBkp_Click);
+            // 
+            // btnFileBkp
+            // 
+            this.btnFileBkp.Location = new System.Drawing.Point(295, 32);
+            this.btnFileBkp.Name = "btnFileBkp";
+            this.btnFileBkp.Size = new System.Drawing.Size(95, 23);
+            this.btnFileBkp.TabIndex = 11;
+            this.btnFileBkp.Text = "Gerar Backup";
+            this.btnFileBkp.UseVisualStyleBackColor = true;
+            this.btnFileBkp.Click += new System.EventHandler(this.btnFileBkp_Click);
+            // 
+            // lblFiles
+            // 
+            this.lblFiles.AutoSize = true;
+            this.lblFiles.Location = new System.Drawing.Point(13, 153);
+            this.lblFiles.Name = "lblFiles";
+            this.lblFiles.Size = new System.Drawing.Size(13, 13);
+            this.lblFiles.TabIndex = 12;
+            this.lblFiles.Text = "_";
+            // 
+            // cmbBkpTime
+            // 
+            this.cmbBkpTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBkpTime.FormattingEnabled = true;
+            this.cmbBkpTime.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "4",
+            "8",
+            "12",
+            "24",
+            "48",
+            "96",
+            "360",
+            "720"});
+            this.cmbBkpTime.Location = new System.Drawing.Point(198, 113);
+            this.cmbBkpTime.Name = "cmbBkpTime";
+            this.cmbBkpTime.Size = new System.Drawing.Size(82, 21);
+            this.cmbBkpTime.TabIndex = 13;
+            this.cmbBkpTime.SelectedIndexChanged += new System.EventHandler(this.cmbBkpTime_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Próximo backup automatico (horas):";
+            // 
+            // frmBackup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 184);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbBkpTime);
+            this.Controls.Add(this.lblFiles);
+            this.Controls.Add(this.btnFileBkp);
+            this.Controls.Add(this.btnDataBkp);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtBakPath);
+            this.Controls.Add(this.txtFilesPath);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblBakFiles);
+            this.Controls.Add(this.lblBakData);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmBackup";
+            this.Text = "GIPP Backup";
+            this.Load += new System.EventHandler(this.frmBackup_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void ContextMenuStrip_ItemClicked(object sender, System.Windows.Forms.ToolStripItemClickedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblBakData;
+        private System.Windows.Forms.Label lblBakFiles;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFilesPath;
+        private System.Windows.Forms.TextBox txtBakPath;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDataBkp;
+        private System.Windows.Forms.Button btnFileBkp;
+        private System.Windows.Forms.Label lblFiles;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbBkpTime;
+    }
+}
+
---
+++ GIPPBackup/frmBackup.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using MySql.Data;
+
+namespace GIPPBackup
+{
+    public partial class frmBackup : Form
+    {
+
+        MySql.Data.MySqlClient.MySqlConnection connection = new MySql.Data.MySqlClient.MySqlConnection("server=192.168.0.221; user id=admin; password=admin; database=pegpese;");
+        MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand();
+        string vTime;
+        Thread t; //Thread que atualiza a lblFiles durante o backup.
+        Thread u; //Thread que verifica se o Backup foi efetuado.
+        Thread v; //Thread que verifica se o Backup do MySQL foi concluido.
+        Boolean vError; //Ferifica se o Backup falhou antes de tentar atualizar as informações de data no banco de dados.
+        Boolean vWait = true; //Aguarda backup do banco antes do backup de arquivos.
+
+        public frmBackup()
+        {
+            InitializeComponent();
+            
+            this.notifyIcon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(); //Cria e configura o icone de notificação do Backup.
+            this.notifyIcon.ContextMenuStrip.Items.Add("Mostrar");
+            this.notifyIcon.ContextMenuStrip.Items.Add("Ocultar");
+            this.notifyIcon.ContextMenuStrip.Items.Add("Fechar");
+            this.notifyIcon.ContextMenuStrip.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.notifyIcon_MenuClick);
+            
+            lblFiles.Text = "Verificando backup...";
+
+            MySql.Data.MySqlClient.MySqlConnection connection = new MySql.Data.MySqlClient.MySqlConnection("server=192.168.0.221; user id=admin; password=admin; database=pegpese;");
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand();
+            cmd.Connection = connection;
+            vError = false;
+            u = new Thread(uThreadProcess);
+            cmbBkpTime.Text = "24"; //A Thread 'u' será iniciada dentro de cmbBkpTime_SelectedIndexChanged.
+            vTime = "24";
+
+            try
+            {
+                connection.Open();
+                cmd.CommandText= "SELECT data_last_backup FROM system_checks WHERE id = '1'";
+                lblBakData.Text= (string)cmd.ExecuteScalar();
+                cmd.CommandText = "SELECT system_last_backup FROM system_checks WHERE id = '1'";
+                lblBakFiles.Text = (string)cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro! " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+
+    private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible=true;
+        }
+
+        private void notifyIcon_MenuClick(object sander, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem.Text == "Mostrar")
+            {
+                this.Visible=true;
+            }
+            if (e.ClickedItem.Text == "Ocultar")
+            {
+                this.Visible = false;
+            }
+            if (e.ClickedItem.Text == "Fechar")
+            {
+                const string message = "Desativar o Backup?";
+                const string caption = "Fechando";
+                
+                if (MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                {
+                    u.Abort();
+                    base.Dispose(true);
+                }
+            }
+        }
+
+        private void frmBackup_Load(object sender, EventArgs e)
+        {
+            try
+            {   // Abrir o arquivo para ler.
+                using (StreamReader sr = new StreamReader(@"C:\Program Files (x86)\GIPP Backup\bakpath.txt"))
+                {
+                    // Ler o arquivo para uma string.
+                    String line = sr.ReadToEnd();
+
+                    txtBakPath.Text = line;
+                }
+                using (StreamReader sr = new StreamReader(@"C:\Program Files (x86)\GIPP Backup\filespath.txt"))
+                {
+                    // Ler o arquivo para uma string.
+                    String line = sr.ReadToEnd();
+                    
+                    txtFilesPath.Text = line;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Arquivo não pode ser lido: " + ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {   // Abrir o arquivo para escrever.
+                using (StreamWriter sr = new StreamWriter(@"C:\Program Files (x86)\GIPP Backup\bakpath.txt"))
+                {
+                    // Grava o texto para um arquivo.
+                    sr.Write("");
+                    sr.Write(txtBakPath.Text);
+                }
+                using (StreamWriter sr = new StreamWriter(@"C:\Program Files (x86)\GIPP Backup\filespath.txt"))
+                {
+                    sr.Write("");
+                    sr.Write(txtFilesPath.Text);
+                }
+                MessageBox.Show("Caminhos atualizados!", "Informação!",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Arquivo não pode ser lido: " + ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnFileBkp_Click(object sender, EventArgs e)
+        {
+            t = new Thread(tThreadProcess);
+            t.Start();
+        }
+
+        private void tThreadProcess()
+        {
+            try
+            {
+                string today = DateTime.Today.ToShortDateString();
+                today = today.Replace("/", "-");
+                string sourcePath = @"" + txtFilesPath.Text;
+                string targetPath = @"" + txtBakPath.Text + @"GIPP_Backup_" + today;
+
+                if (!System.IO.Directory.Exists(targetPath))
+                {
+                    System.IO.Directory.CreateDirectory(targetPath);
+                }
+
+                DirectoryCopy(sourcePath, targetPath, true);
+
+                if (vError == false)
+                {
+                    this.lblFiles.BeginInvoke((MethodInvoker)delegate () { this.lblFiles.Text = "Backup Completo!"; }); //Permite o acesso aos objetos da Thread principal sem risco de Deadlocks.
+                    today = today.Replace("-", "/");
+                    CreateMySqlCommand("UPDATE `pegpese`.`system_checks` SET `system_last_backup`='" + today + "' WHERE `id`='1'", connection);
+                    this.lblBakFiles.BeginInvoke((MethodInvoker)delegate () { this.lblBakFiles.Text = today; });
+                }
+                else
+                {
+                    this.lblFiles.BeginInvoke((MethodInvoker)delegate () { this.lblFiles.Text = "Backup falhou!"; });
+                }
+                vError = false;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.lblFiles.BeginInvoke((MethodInvoker)delegate () { this.lblFiles.Text = "Backup falhou!"; });
+            }
+            
+        }
+
+        private void uThreadProcess()
+        {
+            try
+            {
+                Thread.Sleep(10000); //Evita que esta Thread tente acessar lblFiles da Thread principal antes que lblFiles seja criado.
+                v = new Thread(vThreadProcess);
+                v.Start();
+
+                int i = 10;
+                while (i > 0)
+                {
+                    this.lblFiles.BeginInvoke((MethodInvoker)delegate () { this.lblFiles.Text = "Verificando backup... " + i.ToString(); });
+                    Thread.Sleep(1000);
+                    i -= 1;
+                }
+
+                while (true) //Este Busy Waiting mantem o Thread de teste rodando constantemente para saber se é hora de um novo backup.
+                {
+                    DateTime dt = Convert.ToDateTime(CreateMySqlSelect("SELECT system_last_backup FROM system_checks WHERE id = '1'", connection));
+                    if (DateTime.Today.Date > dt)
+                    {
+                        v = new Thread(vThreadProcess);
+                        v.Start();
+                        this.lblFiles.BeginInvoke((MethodInvoker)delegate () { vWait = true; });
+                        
+                        while (vWait == true)
+                        {
+                            Thread.Sleep(1000);
+                        }
+
+                        t = new Thread(tThreadProcess);
+                        t.Start();
+                    }
+                    if (DateTime.Today.Date == dt)
+                    {
+                        this.lblFiles.BeginInvoke((MethodInvoker)delegate () { this.lblFiles.Text = "Backup de hoje já foi concluido!"; });
+                    }
+                    int vTimeI = Convert.ToInt16(vTime);
+
+                    vTimeI = (vTimeI * 60 * 60 * 1000);
+                    //vTimeI=(10000);
+                    Thread.Sleep(vTimeI); //A Thread dorme por um certo periodo determinado pelo cmbBkpTime.
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERRO!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void vThreadProcess()
+        {
+            try
+            {
+                string today = DateTime.Today.ToShortDateString();
+                today = today.Replace("/", "-");
+                string vCommand = "";
+                System.Diagnostics.Process.Start(@"C:\Program Files (x86)\GIPP Backup\mysql.bat", vCommand);
+                Thread.Sleep(5000);
+
+                FileInfo file = new FileInfo(@"C:\GIPP\GIPP\MySQL\" + today + ".sql");
+
+                if (!file.Exists)
+                {
+                    throw new DirectoryNotFoundException(
+                        "Arquivo de backup de dados não foi criado");
+                }
+                else
+                {
+                    today = today.Replace("-", "/");
+                    CreateMySqlCommand("UPDATE `pegpese`.`system_checks` SET `data_last_backup`='" + today + "' WHERE `id`='1'", connection);
+                    this.lblBakData.BeginInvoke((MethodInvoker)delegate () { this.lblBakData.Text = today; });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            this.lblFiles.BeginInvoke((MethodInvoker)delegate () { vWait = false; });
+        }
+
+        public void CreateMySqlCommand(string myExecuteQuery, MySql.Data.MySqlClient.MySqlConnection myConnection)
+        {
+            try
+            {
+            MySql.Data.MySqlClient.MySqlCommand myCommand = new MySql.Data.MySqlClient.MySqlCommand(myExecuteQuery, myConnection);
+            myCommand.Connection.Open();
+            myCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+
+        public string CreateMySqlSelect(string myExecuteQuery, MySql.Data.MySqlClient.MySqlConnection myConnection)
+        {
+            try
+            {
+                connection.Open();
+                cmd.CommandText = myExecuteQuery;
+                cmd.Connection = myConnection;
+                return (string)cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro! " + ex.Message);
+                return "";
+            }
+            finally
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+
+        private void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            try
+            {
+                // Get the subdirectories for the specified directory.
+                DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+                if (!dir.Exists)
+                {
+                    throw new DirectoryNotFoundException(
+                        "Source directory does not exist or could not be found: "
+                        + sourceDirName);
+                }
+
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                // If the destination directory doesn't exist, create it.
+                if (!Directory.Exists(destDirName))
+                {
+                    Directory.CreateDirectory(destDirName);
+                }
+
+                // Get the files in the directory and copy them to the new location.
+                FileInfo[] files = dir.GetFiles();
+
+                foreach (FileInfo file in files)
+                {
+                    string temppath = Path.Combine(destDirName, file.Name);
+                    file.CopyTo(temppath, false);
+                    if (this.lblFiles.InvokeRequired)
+                    {
+                        this.lblFiles.BeginInvoke((MethodInvoker)delegate () { this.lblFiles.Text = file.ToString(); });
+                    }
+                    else
+                    {
+                        this.lblFiles.Text = "";
+                    }
+                }
+
+                // If copying subdirectories, copy them and their contents to new location.
+                if (copySubDirs)
+                {
+                    foreach (DirectoryInfo subdir in dirs)
+                    {
+                        string temppath = Path.Combine(destDirName, subdir.Name);
+                        DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                        this.lblFiles.BeginInvoke((MethodInvoker)delegate () { this.lblFiles.Text = subdir.ToString(); });
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show( ex.Message,"ERRO",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                vError = true;
+            }
+        }
+
+        private void cmbBkpTime_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblFiles.Text = "Aguarde...";
+            try
+            {
+                u.Abort();
+                u = new Thread(uThreadProcess);
+                u.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDataBkp_Click(object sender, EventArgs e)
+        {
+            v = new Thread(vThreadProcess);
+            v.Start();
+        }
+    }
+}
